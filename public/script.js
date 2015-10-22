0d1b8695d3a47eb43e372459b3bdb2e7672f3610
tim Trousdale <timtrousdale@gmail.com>
2015-10-22 10:03:59 -0600

    added test branch

---
+++ public/script.js
@@ -0,0 +1,73 @@
+/* globals $, document, io */
+
+$(document).ready(function () {
+    var name = "";
+
+    $(".submit").click(function () {
+        name = $(".userName").val();
+        $("#data").attr('placeholder', 'send message as ' + name);
+        $(".textTyper").css('visibility', 'visible');
+         socket.emit('adduser', name);
+
+    });
+
+    $('#userName').keypress(function (e) {
+        if (e.which == 13) {
+            name = $(".userName").val();
+            $("#data").attr('placeholder', 'send message as ' + name);
+            $(".textTyper").css('visibility', 'visible');
+             socket.emit('adduser', name);
+        }
+    });
+
+        function escaped(s) {
+        return $("<div></div>").html(s).html();
+    }
+
+    var socket = io.connect('/');
+
+    // on connection to server, ask for user's name with an anonymous callback
+    socket.on('connect', function () {
+        // call the server-side function 'adduser' and send one parameter (value of prompt)
+        console.log('socket connected');
+    });
+
+
+    // listener, whenever the server emits 'updatechat', this updates the chat body
+    socket.on('updatechat', function (username, data) {
+        $('#conversation').append('<b>' + escaped(username) + ':</b> ' + escaped(data) + "<br/>");
+    });
+
+    // listener, whenever the server emits 'updateusers', this updates the username list
+    socket.on('updateusers', function (data) {
+        $('#users').empty();
+        $.each(data, function (key, value) {
+            $('#users').append('<div><a href="' + searchUrlFor(key) + '" target="_blank">' + key + '</div>');
+        });
+    });
+
+    socket.on('servernotification', function (data) {
+        var searchUrl = searchUrlFor(data.username);
+        if (data.connected) {
+            if (data.to_self) data.username = "you";
+
+            $('#conversation').append('connected: <a href="' + searchUrl + '" target="_blank">' + escaped(data.username) + "</a><br/>");
+        } else {
+            $('#conversation').append('disconnected: <a href="' + searchUrl + '" target="_blank">' + escaped(data.username) + "</a><br/>");
+        }
+    });
+
+    // on load of page
+    $(function () {
+        // when the client hits ENTER on their keyboard
+        $('#data').keypress(function (e) {
+            if (e.which == 13) {
+                var message = $('#data').val();
+                $('#data').val('');
+                // tell server to execute 'sendchat' and send along one parameter
+                socket.emit('sendchat', message);
+            }
+        });
+    });
+
+});
